2d2fa00fa141e5dc6c6ab2f6eb767e7327e1d138
takumihavetiyou <takumisuma@icloud.coma>
2023-06-26 15:03:45 +0900

    mainvisual実装

--- src/modules/home/Mainvisual.tsx
+++ src/modules/home/Mainvisual.tsx
@@ -1,9 +1,13 @@
 import { Box, SxProps, Theme } from "@mui/material";
+import Mainvisualimg  from "../../resources/home/mainvisual_backgraound.png";
 
 export const Mainvisual = () => {
   return (
     <Box className="Mainvisual" sx={sx}>
-      フッター
+        <Box className="img1">
+        <Box className="text1">安心<span　className="span1">と</span>信頼<span className="span1">✖︎</span><br></br>高度<span className="span1">な</span>技術力</Box>
+        <Box className="text2">高品質な開発を標準に。</Box>
+        </Box>
     </Box>
   );
 };
@@ -11,5 +15,28 @@ export const Mainvisual = () => {
 const sx: SxProps<Theme> = {
   "&.Mainvisual" : {
     width: "100%",
+  },
+  ".img1": {
+    backgroundImage: `url(${Mainvisualimg})`,
+    height: 700,
+  },
+  ".text1": {
+    color: "#ffffff",
+    paddingTop:"340px",
+    paddingLeft:"100px",
+    fontSize: "80px",
+    fontWeight: 500,
+    fontFamily:"Noto Sans JP",
+  },
+  ".text2": {
+    color: "#ffffff",
+    paddingLeft:"100px",
+    fontSize:"60px",
+    fontFamily:"Noto Sans JP",
+    fontStyle:"normal",
+    fontWeight: 500,
+  },
+  ".span1": {
+    fontSize:"40px",
   },
 };
